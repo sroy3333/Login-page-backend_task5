4953ef8f659d0845570f64da0471aacd092c29cd
sroy3333 <121767970+sroy3333@users.noreply.github.com>
2024-04-02 01:49:24 +0530

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -24,6 +24,30 @@ const signup = async (req, res) => {
     }
 }
 
+const login = async (req, res) => {
+    try {
+    const { email, password } = req.body;
+    if(isstringinvalid(email) || isstringinvalid(password)) {
+        return res.status(400).json({message: 'Email id or password is missing', success: false})
+    }
+    console.log(password);
+    const user = await User.findAll({ where : {email} })
+    if(user.length > 0) {
+        if(user[0].password === password) {
+            res.status(200).json({success: true, message: 'User logged in successfuly'})
+        } else {
+            return res.status(400).json({success: false, message: 'password is incorrect'})
+        }
+    } else {
+        return res.status(404).json({success: false, message: 'User does not exist'})
+    }
+    }catch(err) {
+        res.status(500).json({message: err, success: false})
+    }
+}
+
 module.exports = {
     signup: signup
+     login: login
+
 };
