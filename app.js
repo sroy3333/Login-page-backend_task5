706e4e4c03e00c08046c4fe7c039ee9055b9a6a3
sroy3333 <121767970+sroy3333@users.noreply.github.com>
2024-04-02 01:58:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,39 +1,33 @@
 const path = require('path');
-
 const express = require('express');
-
 const sequelize = require('./util/database');
-
 const userRoutes = require('./routes/user');
-
-const authRoutes = require('./routes/auth');
-
 const User = require('./models/users');
-
-var cors = require('cors');
+const cors = require('cors');
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
-
 app.use('/user', userRoutes);
 
-app.use('/auth', authRoutes);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'signup.html'));
+app.get('/login/login.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'login.html'));
 });
 
+
+app.get('/signup/signup.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'signup.html'));
+});
 
 sequelize
-.sync()
-.then(() => {
+  .sync()
+  .then(() => {
     app.listen(5000);
-})
-.catch(err => {
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
